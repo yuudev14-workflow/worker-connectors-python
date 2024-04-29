2493893e2eae045a6980cac80b05c08fc9d8ebfb
yuudev14 <takaki.yu.dev@gmail.com>
2024-04-29 23:13:30 +0900

    setup rabbitmq

--- main.py
+++ main.py
@@ -1,3 +1,56 @@
 """
 main file
 """
+
+import asyncio
+import aio_pika
+import aio_pika.abc
+
+
+async def main(loop):
+    # Connecting with the given parameters is also possible.
+    # aio_pika.connect_robust(host="host", login="login", password="password")
+    # You can only choose one option to create a connection, url or kw-based params.
+    connection = await aio_pika.connect_robust(
+        "amqp://guest:guest@127.0.0.1/", loop=loop
+    )
+
+    async with connection:
+        queue_name = "workflow"
+
+        # Creating channel
+        channel: aio_pika.abc.AbstractChannel = await connection.channel()
+
+        # Declaring queue
+        queue: aio_pika.abc.AbstractQueue = await channel.declare_queue(
+            queue_name,
+            durable=True,
+            auto_delete=False,
+            exclusive=False,
+        )
+
+        queue2: aio_pika.abc.AbstractQueue = await channel.declare_queue(
+            "workflow_processor",
+            durable=True,
+            auto_delete=False,
+            exclusive=False,
+        )
+
+        async with queue.iterator() as queue_iter:
+            # Cancel consuming after __aexit__
+            async for message in queue_iter:
+                async with message.process():
+                    print(message.body)
+                    await channel.default_exchange.publish(
+                        aio_pika.Message(body=str(message.body).encode()),
+                        routing_key=queue2.name,
+                    )
+
+                    if queue.name in message.body.decode():
+                        break
+
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main(loop))
+    loop.close()
--- src/core/core.py
+++ src/core/core.py
@@ -4,7 +4,7 @@ main file
 
 # things i need to do
 # 1. Format a json for the playbooks nodes and vertices 
-# 2. TODO: Create a function that update the json into a graph
+# 2. DONT DO: Create a function that update the json into a graph
 # 3. TODO: Function that checks if graph is acyclical
 # 4. TODO: Create a function for bfs
 # 5. TODO: Connect to message queue
@@ -13,18 +13,80 @@ main file
 # 8. TODO: Create a function that runs a connectors actions specified in the message queue
 
 sample_json = {
-  "name": "playbook_1",
-  "description": "tests",
-  "created_at": "tests",
-  "created_by": "tests",
-  "nodes": [
-    {
-      "id": "1shvkfsd",
-      "description": "adasd"
-    }
-  ],
+    "name": "playbook_1",
+    "description": "tests",
+    "created_at": "tests",
+    "created_by": "tests",
+    "nodes": [
+        {
+            "id": "1shvkfsd",
+            "description": "adasd"
+        }
+    ],
 
-  "workflow": {
-    "id_1": []
-  }
+    "workflow": {
+        "id_1": [] # list of ids its connected
+    }
 }
+
+
+
+from collections import deque
+
+def bfs(node: str):
+    visit = set()
+    queue = deque()
+    visit.add(node)
+    queue.append(node)
+
+    while queue:
+        for _ in range(len(queue)):
+            curr = queue.popleft()
+            for neighbor in graph[curr]:
+                if neighbor not in visit:
+                    visit.add(neighbor)
+                    queue.append(neighbor)
+
+
+def is_acyclic_graph(graph: dict):
+    visit: set = set()
+    stack: set = set()
+
+    def dfs(node: str):
+        if node in stack:
+            return True
+        
+        if node in visit:
+            return False
+        
+        visit.add(node)
+        stack.add(node)
+
+
+        for neighbor in graph[node]:
+            if dfs(neighbor):
+                return True
+            
+        stack.remove(node)
+        return False
+
+
+
+    for node in graph:
+        if dfs(node):
+            return True
+        
+    return False
+
+
+# check for acyclical graphs
+graph = {
+    'A': ['B', 'C'],
+    'B': ['D'],
+    'C': ['E', 'D'],
+    'D': [],
+    'E': ['F'],
+    'F': []
+}
+
+print(is_acyclic_graph(graph))
