169cc78af958e4a16706a58e100b0b6daf0186ce
yuudev14 <takaki.yu.dev@gmail.com>
2024-04-29 23:18:50 +0900

    init commit setup rabbitmq

--- main.py
+++ main.py
@@ -6,48 +6,45 @@ import asyncio
 import aio_pika
 import aio_pika.abc
 
+from src import constants
+
 
 async def main(loop):
     # Connecting with the given parameters is also possible.
     # aio_pika.connect_robust(host="host", login="login", password="password")
     # You can only choose one option to create a connection, url or kw-based params.
     connection = await aio_pika.connect_robust(
-        "amqp://guest:guest@127.0.0.1/", loop=loop
+       constants.MQ_URL, loop=loop
     )
 
     async with connection:
-        queue_name = "workflow"
-
         # Creating channel
         channel: aio_pika.abc.AbstractChannel = await connection.channel()
 
         # Declaring queue
-        queue: aio_pika.abc.AbstractQueue = await channel.declare_queue(
-            queue_name,
+        worlflow_queue: aio_pika.abc.AbstractQueue = await channel.declare_queue(
+            constants.WORKFLOW_QUEUE,
             durable=True,
             auto_delete=False,
             exclusive=False,
         )
 
-        queue2: aio_pika.abc.AbstractQueue = await channel.declare_queue(
-            "workflow_processor",
+        workflow_processor_queue: aio_pika.abc.AbstractQueue = await channel.declare_queue(
+            constants.WORKFLOW_PROCESSOR_QUEUE,
             durable=True,
             auto_delete=False,
             exclusive=False,
         )
 
-        async with queue.iterator() as queue_iter:
+        async with worlflow_queue.iterator() as queue_iter:
             # Cancel consuming after __aexit__
             async for message in queue_iter:
                 async with message.process():
                     print(message.body)
                     await channel.default_exchange.publish(
                         aio_pika.Message(body=str(message.body).encode()),
-                        routing_key=queue2.name,
+                        routing_key=workflow_processor_queue.name,
                     )
-
-                    if queue.name in message.body.decode():
-                        break
 
 
 if __name__ == "__main__":
